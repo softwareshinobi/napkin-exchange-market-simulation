0760c5d65450c985d3e2d79512279a3f616020df
Software Shinobi <the.software.shinobi@gmail.com>
2024-01-04 02:28:52 -0500

    automated terminal push

--- src/main/java/digital/softwareshinobi/napkinexchange/account/model/entity/LimitOrderType.java
+++ src/main/java/digital/softwareshinobi/napkinexchange/account/model/entity/LimitOrderType.java
@@ -2,14 +2,10 @@ package digital.softwareshinobi.napkinexchange.account.model.entity;
 
 public enum LimitOrderType {
 
- //   LONG_BUY_MARKET_PRICE,
-    
-    LONG_BUY_LIMIT_PRICE,    
-    
-  //  LONG_SELL_MARKET_PRICE, 
-    
+    LONG_BUY_LIMIT_PRICE,
+
     LONG_SELL_STOP_LOSS,
     
-    LONG_SELL_TAKE_PROFIT,       
+    LONG_SELL_TAKE_PROFIT,
 
 }
